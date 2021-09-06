28b05013b389aa1c34e57da84ed94e97bbcde923
Amit Kulkarni <>
2021-09-06 15:28:57 -0500

    no message

--- TESTAMIT.java
+++ TESTAMIT.java
@@ -1,13 +1,7 @@
 //import java.io.IOException;
 
-import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 import static java.lang.System.*;
-
+import java.util.Date;
 
 
 public class TESTAMIT {
